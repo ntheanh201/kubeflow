8d44992d841d36b457c424597f4a712a78f83ff0
The Anh Nguyen <ntheanh201@gmail.com>
2025-06-12 18:48:23 +0200

    feat: add GPU Fractional on Notebook overview
    Signed-off-by: The Anh Nguyen <ntheanh201@gmail.com>

--- components/crud-web-apps/jupyter/frontend/src/app/types/environmentVariablesGroup.ts
+++ components/crud-web-apps/jupyter/frontend/src/app/types/environmentVariablesGroup.ts
@@ -2,5 +2,7 @@ import { PodDefault } from 'src/app/types';
 import { VariablesGroup } from 'kubeflow';
 
 export interface EnvironmentVariablesGroup extends VariablesGroup {
+  name: string;
+  chipsList?: any[];
   configuration?: PodDefault;
 }
